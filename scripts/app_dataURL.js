2f89dcb86d07c0e59909a0ccbbab68a5a413a96c
masarabbit <masahitoart@gmail.com>
2022-03-26 15:01:26 +0000

    :refactor

--- scripts/app_dataURL.js
+++ scripts/app_dataURL.js
@@ -1,95 +1,66 @@
 function init() {
 
-  const combineCanvas = document.querySelector('#combineCanvas')
-  const context = combineCanvas.getContext('2d')
+  const canvas = document.querySelector('canvas')
+  const ctx = canvas.getContext('2d')
   const imageWrapper = document.querySelector('.image_wrapper')
-  // const imageTarget = document.createElement('img')
-  const outputButton = document.querySelector('#output')
-  const outputFromDataButton = document.querySelector('#output_from_data')
-  const textOutput = document.querySelectorAll('.text_output')
+  const buttons = document.querySelectorAll('button')
+  const textOutput = document.querySelectorAll('textarea')
   const dataInput = document.querySelector('#data_input')
 
+  const updateImage = (data, img) =>{
+    imageWrapper.innerHTML = ''
+    img.src = data
+    imageWrapper.appendChild(img)
+  }
 
-
-  function output(){
-
+  const output = () =>{
     const uploadFile = document.getElementById('uploadFile')
     const uploadedFiles = uploadFile.files[0]
     const blobURL = window.URL.createObjectURL(uploadedFiles)
     const imageTarget = new Image()
-    // console.log({ uploadedFiles })
-    // console.log({ imageTarget })
-
-    let height
-    let width
     
+    imageTarget.onload = () => {
+      const { naturalWidth: w, naturalHeight: h } = imageTarget
+      canvas.width = w
+      canvas.height = h 
+      ctx.drawImage(imageTarget, 0, 0, w, h)
 
-    imageTarget.addEventListener('load', function() {
-      width = imageTarget.naturalWidth 
-      height = imageTarget.naturalHeight 
-      // context.drawImage(imageTarget, 0, 0, width, height)
-      // context.drawImage(imageTarget, 0, 0)
-      combineCanvas.setAttribute('width', width)
-      combineCanvas.setAttribute('height', height) 
-      // combineCanvas.setAttribute('width', 200)
-      // combineCanvas.setAttribute('height', 200 * (height / width)) 
-    }, false)
-
-
-
-    imageWrapper.innerHTML = ''
-    imageTarget.src = blobURL
-    imageWrapper.appendChild(imageTarget)
-
-
-    setTimeout(()=>{
-      // context.drawImage(imageTarget, 0, 0, 200, 200 * (height / width))
-      context.drawImage(imageTarget, 0, 0, width, height)
-      textOutput[0].style.padding = '10px'
-      textOutput[0].innerHTML = combineCanvas.toDataURL()
-
-      // const svgString = combineCanvas.innerHTML
-      // console.log(svgString)
-      // const dataUrl = 'data:image/svg+xml,'+encodeURIComponent(svgString);
-      // textOutput[0].innerHTML = dataUrl
+      textOutput[0].innerHTML = canvas.toDataURL()
+      const boxHeight = textOutput[0].scrollHeight || 0
+      textOutput[0].style.height = `${boxHeight}px`
+      buttons[1].classList.remove('display_none')
       
-      // var imgData = context.getImageData(0, 0, 200, 200 * (height / width))
-      var imgData = context.getImageData(0, 0, width, height)
-      textOutput[1].style.padding = '10px'
-      textOutput[1].innerHTML = imgData.data
-      console.log(imgData)
-    },200)
+      // const imgData = context.getImageData(0, 0, w, h)
+      // textOutput[1].style.padding = '10px'
+      // textOutput[1].innerHTML = imgData.data
+    }
+
+    updateImage(blobURL, imageTarget)
   }
   
 
-  function outputImage(){
-    const imageTarget = new Image()
-
-    let height
-    let width
+  const outputImage = () =>{
+    const imageTarget = new Image()    
     
-    imageTarget.addEventListener('load', function() {
-      width = imageTarget.naturalWidth 
-      height = imageTarget.naturalHeight 
+    imageTarget.onload = () => {
+      const { naturalWidth: w, naturalHeight: h } = imageTarget
+      canvas.width = w
+      canvas.height = h 
+      ctx.drawImage(imageTarget, 0, 0, w, h)
+    }
 
-      // combineCanvas.setAttribute('width', 200)
-      // combineCanvas.setAttribute('height', 200 * (height / width)) 
-      combineCanvas.setAttribute('width', width)
-      combineCanvas.setAttribute('height', height) 
-    }, false)
-
-    imageWrapper.innerHTML = ''
-    imageTarget.src = dataInput.value
-    imageWrapper.appendChild(imageTarget)
-
-    setTimeout(()=>{
-      // context.drawImage(imageTarget, 0, 0, 200, 200 * (height / width))
-      context.drawImage(imageTarget, 0, 0, width, height)
-    },200)
+    updateImage(dataInput.value, imageTarget)
+  }
+  
+  const copyText = box =>{
+    box.select()
+    box.setSelectionRange(0, 99999) // For mobile devices 
+    document.execCommand('copy')
   }
 
-  outputButton.onclick = output
-  outputFromDataButton.onclick = outputImage
+  buttons[0].onclick = output
+  buttons[1].addEventListener('click', ()=> copyText(textOutput[0]))
+  buttons[2].onclick = outputImage
 }
 
 window.addEventListener('DOMContentLoaded', init)
